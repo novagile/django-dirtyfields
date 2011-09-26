6e45bed9f0ded99866421fff96baf824e1954a43
Ashley Camba Garrido <ashwoods@gmail.com>
2011-09-26 16:15:13 +0200

    testing

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     long_description = open('README.rst','r').read(),
     author = 'Simon de Haan',
     packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    install_requires = listify('requirements.pip'),
-    classifiers = listify('CLASSIFIERS.txt')
+#    package_dir = {'': 'src'},
+#    install_requires = listify('requirements.pip'),
+#    classifiers = listify('CLASSIFIERS.txt')
 )
 
